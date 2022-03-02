9dbb4c887da54e64531611a0c1ec4e8f75d0bfa8
Nitin Verma <23135333@AFRICA.AIRTEL.ITM>
2022-03-02 15:17:43 +0530

    added message

--- src/app/landing/meetups/meetup/meetup.component.ts
+++ src/app/landing/meetups/meetup/meetup.component.ts
@@ -11,18 +11,24 @@ import { MeetingsService } from "src/app/services/meetings";
 export class Meetup{
     @Input() meetup:any;
     flag=false;
+    ifExists=false;
     constructor(private validate:ValidationService,
                 private router:Router,
                 private _meetup:MeetingsService){    }
     
     addToFavorite(){
-         if(this.validate.userToken.length<1){
+        if(this.validate.userToken.length<1){
              this.router.navigateByUrl('/login');
              this.flag=false;
          }
-         else{
-            this._meetup.addToFavorite(this.meetup)
-            this.flag=true
+        else{
+             if(this._meetup.checkIfExists(this.meetup)){
+                 this.ifExists=true;
+             }
+             else{
+                this._meetup.addToFavorite(this.meetup)
+                this.flag=true
+            }
         }
     }
 
